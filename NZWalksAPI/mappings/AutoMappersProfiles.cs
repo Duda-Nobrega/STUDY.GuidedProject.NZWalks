6725329bb691c798766a0f669a559d8adeb535c3
Maria Eduarda Nóbrega Santos <maria.santos@gestqual.com.br>
2025-02-18 15:07:21 -0300

    Inicio da implementação do Automapper

---
+++ NZWalksAPI/mappings/AutoMappersProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+
+namespace NZWalksAPI.mappings
+{
+    public class AutoMappersProfiles : Profile
+    {
+        public AutoMappersProfiles()
+        {
+            CreateMap<UserDto, UserDomain>().ReverseMap();
+        }
+    }
+
+    public class UserDto
+    {
+        public string FullName { get; set; }
+    }
+
+    public class UserDomain
+    {
+        public string FullName { get; set; }
+    }
+}
